a60f274e0f31bb3f8daea99decef9426affa322c
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2021-10-20 23:57:03 +0900

    build : Initial commit

---
+++ app/src/main/java/com/dongyang/android/pcheduler/MainActivity.kt
@@ -0,0 +1,13 @@
+// 21.10.16 First Commit
+
+package com.dongyang.android.pcheduler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
